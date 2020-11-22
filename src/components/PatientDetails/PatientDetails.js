fc80b1d581859f06fd3ff495fc1ae86f67dc655f
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-22 10:42:02 -0500

    fetch patient details

--- src/components/PatientDetails/PatientDetails.js
+++ src/components/PatientDetails/PatientDetails.js
@@ -1,9 +1,79 @@
 import React from 'react'
+import { Component } from 'react'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import useStyles from './PatientDetailsStyle'
+import { TextField } from '@material-ui/core'
+import { Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+import { connect } from 'react-redux'
+import { ptLoginAction } from '../../redux/actions';
+import { withRouter } from 'react-router-dom'
 
-const PatientDetails = (props) => {
-    return (
-        <h1>{props.id}</h1>
-    )
+
+
+class PatientDetails extends Component {
+
+    state = {
+        patient_id: this.props.id,
+        patient: null
+    }
+
+    
+
+    componentDidMount() {
+        const configObj = {
+            method: "GET",
+            headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
+        }
+        fetch(`http://localhost:3000/api/v1/patients/${this.state.patient_id}`, configObj)
+        .then(res => res.json())
+        .then(res => this.setState({ patient: res }))
+    }
+
+
+
+    render() {
+        const { classes } = this.props
+        return (
+            <div > 
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                    <Grid container spacing={3} align="center" justify="center" >
+                        <Grid item xs={9} >
+                        <Paper className={classes.loginBox}>
+                        <Typography component="span">
+                            <h1>{this.state.patient_id}</h1>
+                            {/* <Card className={classes.root}>
+                            <CardContent>
+                                <h3>Log In: Patient </h3>
+                                <TextField onChange={this.formEdit} className={classes.textField} value={this.state.email} name="email" type="text" label="email"/>
+                                <TextField onChange={this.formEdit} className={classes.textField} value={this.state.password} name="password" type="password" label="password" ></TextField>
+                                <br/><br/><Button type="submit" onClick={this.submitHandler}>Log in</Button>
+                            </CardContent>
+                            </Card> */}
+                        </Typography>
+                        </Paper>
+                        </Grid>   
+                    </Grid>
+                    
+                    </Grid>
+                </Grid>
+                </div>
+        )
+    }
 }
 
-export default PatientDetails
+const msp = (state) => {
+    return {doctor: state.doctor}
+}
+
+const mdp = (dispatch) => {
+    return { patientLogin: (pt) => dispatch(ptLoginAction(pt, dispatch))}
+}
+
+export default connect(msp, mdp)(withStyles(useStyles, { withTheme: true })(withRouter(PatientDetails)))
