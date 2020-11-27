119cf3bab89468e3c573093ea32aea085cf0cd00
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-27 16:53:35 -0500

    appt book form

--- src/containers/PtCalendarLanding/PtCalendarLanding.js
+++ src/containers/PtCalendarLanding/PtCalendarLanding.js
@@ -13,6 +13,7 @@ import { Redirect } from 'react-router-dom'
 import { TextField, MenuItem } from '@material-ui/core';
 import PtCalendar from '../../components/PtCalendar/PtCalendar'
 import { withStyles } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 
 
 class PtCalendarLanding extends Component {
@@ -34,14 +35,21 @@ class PtCalendarLanding extends Component {
                         {!this.props.apptDate ? null
                         :
                         <>
-                        <Typography>Appointments available for {this.props.apptDate}</Typography>
-                        <TextField  id="time" label="Choose time" name="time" select>
+                        
+                        {this.props.apptDate.slice(0,3) === "Sat" || this.props.apptDate.slice(0,3) === "Sun" ? "Appointments available Monday-Friday" : 
+                        <>
+                        <Typography>Appointments available for {this.props.apptDate}:</Typography>
+                        <TextField  id="time" name="time" select>
                             <MenuItem value="morning">Morning</MenuItem>
                             <MenuItem value="afternoon ">Afternoon</MenuItem>
                             <MenuItem value="evening ">Evening</MenuItem>
-                        </TextField>
+                        </TextField><br/><br/>
+                        <TextField placeholder="Concerns"></TextField><br/><br/>
+                        <Button variant="outlined" color="primary">Make Appointment</Button><br/>
                         </>
-                        }
+                        
+                        
+                        }</>}
                         </>
 
                     </Paper>
